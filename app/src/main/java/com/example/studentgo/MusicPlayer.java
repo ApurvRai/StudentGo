6b51e319c9add90d8f9f01b720efb42b50dbe1c3
Apurv Rai <raiiapurv23@gmail.com>
2021-10-25 21:46:24 +0530

    musicplayer

--- app/src/main/java/com/example/studentgo/MusicPlayer.java
+++ app/src/main/java/com/example/studentgo/MusicPlayer.java
@@ -2,13 +2,14 @@ package com.example.studentgo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MusicPlayer extends AppCompatActivity {
-    Button b1, b2;
+    Button b1, b2, b3;
     MediaPlayer mp;
 
     @Override
@@ -17,6 +18,7 @@ public class MusicPlayer extends AppCompatActivity {
         setContentView(R.layout.activity_music_player);
         b1 = (Button)findViewById(R.id.button19);
         b2 = (Button)findViewById(R.id.button20);
+        b3 = (Button)findViewById(R.id.button23);
         mp = MediaPlayer.create(this, R.raw.y);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +30,14 @@ public class MusicPlayer extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mp.pause();
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MusicPlayer.this, Homepage.class);
+                startActivity(i);
+                finish();
             }
         });
     }
